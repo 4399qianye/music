6300095e4296fef1e38ba0db405fbbac9acfb85f
4399qianye <101877376+4399qianye@users.noreply.github.com>
2022-09-30 20:51:53 +0800

    Add files via upload

---
+++ src/common/config.js
@@ -0,0 +1,188 @@
+module.exports = {
+  windowSizeList: [
+    {
+      id: 0,
+      name: 'smaller',
+      width: 828,
+      height: 530,
+      fontSize: '14px',
+    },
+    {
+      id: 1,
+      name: 'small',
+      width: 920,
+      height: 590,
+      fontSize: '16px',
+    },
+    {
+      id: 2,
+      name: 'medium',
+      width: 1018,
+      height: 650,
+      fontSize: '16px',
+    },
+    {
+      id: 3,
+      name: 'big',
+      width: 1114,
+      height: 708,
+      fontSize: '17px',
+    },
+    {
+      id: 4,
+      name: 'larger',
+      width: 1202,
+      height: 766,
+      fontSize: '17px',
+    },
+    {
+      id: 5,
+      name: 'oversized',
+      width: 1382,
+      height: 886,
+      fontSize: '18px',
+    },
+    {
+      id: 6,
+      name: 'huge',
+      width: 1686,
+      height: 1062,
+      fontSize: '19px',
+    },
+  ],
+  navigationUrlWhiteList: [],
+  themes: [
+    {
+      id: 0,
+      name: '绿意盎然',
+      className: 'green',
+    },
+    {
+      id: 1,
+      name: '蓝田生玉',
+      className: 'blue',
+    },
+    {
+      id: 2,
+      name: '信口雌黄',
+      className: 'yellow',
+    },
+    {
+      id: 3,
+      name: '橙黄橘绿',
+      className: 'orange',
+    },
+    {
+      id: 4,
+      name: '热情似火',
+      className: 'red',
+    },
+    {
+      id: 10,
+      name: '粉装玉琢',
+      className: 'pink',
+    },
+    {
+      id: 5,
+      name: '重斤球紫',
+      className: 'purple',
+    },
+    {
+      id: 6,
+      name: '灰常美丽',
+      className: 'grey',
+    },
+    {
+      id: 11,
+      name: '青出于黑',
+      className: 'ming',
+    },
+    {
+      id: 13,
+      name: '黑灯瞎火',
+      className: 'black',
+    },
+    {
+      id: 7,
+      name: '月里嫦娥',
+      className: 'mid_autumn',
+    },
+    {
+      id: 8,
+      name: '木叶之村',
+      className: 'naruto',
+    },
+    {
+      id: 9,
+      name: '新年快乐',
+      className: 'happy_new_year',
+    },
+    {
+      id: 14,
+      name: '必应-亮',
+      className: 'bing_light',
+    },
+    {
+      id: 15,
+      name: '必应-暗',
+      className: 'bing_dark',
+    },
+    {
+      id: 16,
+      name: '动漫-亮',
+      className: 'anime_light',
+    },
+    {
+      id: 17,
+      name: '动漫-暗',
+      className: 'anime_dark',
+    },
+    {
+      id: 18,
+      name: '幻境',
+      className: 'eclipse',
+    },
+    {
+      id: 19,
+      name: '星云',
+      className: 'stars',
+    },
+    {
+      id: 20,
+      name: '崩坏3',
+      className: 'bh3',
+    },
+    {
+      id: 21,
+      name: '原神',
+      className: 'ys1',
+    },
+    {
+      id: 22,
+      name: '原神',
+      className: 'ys2',
+    },
+    {
+      id: 23,
+      name: '原神',
+      className: 'ys3',
+    },
+    {
+      id: 24,
+      name: '自定义-亮',
+      className: 'local_light',
+    },
+    {
+      id: 25,
+      name: '自定义-暗',
+      className: 'local_dark',
+    },
+    {
+      id: 26,
+      name: '中秋快乐',
+      className: 'zqkl',
+    },
+  ],
+  themeLights: [14, 0, 1, 2, 3, 4, 10, 5, 6, 11, 7, 8, 9, 16, 18, 19, 20, 24, 26],
+  themeDarks: [13, 7, 15, 17, 25],
+}
